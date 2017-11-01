540507240c22e22ff28b2f3cff51a5573c9592ff
sulealp <32601095+sulealp@users.noreply.github.com>
2017-11-01 14:09:52 +0300

    Add files via upload

--- sourceForge/src/main/java/readAllPages.java
+++ sourceForge/src/main/java/readAllPages.java
@@ -9,7 +9,7 @@ public class readAllPages {
         String url;
         ArrayList<HashMap<String,String>> page = new ArrayList<HashMap<String, String>>();
 
-        for(int pageNumber=0; pageNumber <= 5; pageNumber++) {
+        for(int pageNumber=0; pageNumber <= 4; pageNumber++) {
             url = rootURL+"?page="+Integer.toString(pageNumber+1);
              page.addAll(readerF.getUrlSource(url));
         }
@@ -18,6 +18,9 @@ public class readAllPages {
             System.out.println((i+1)+".Name: "+page.get(i).get("Name"));
             System.out.println("Description: "+page.get(i).get("Description"));
             System.out.println(page.get(i).get("Link"));
+            System.out.println("Features: "+page.get(i).get("Features"));
+            System.out.println("Categories: "+page.get(i).get("Categories"));
+            System.out.println("PL: "+page.get(i).get("PL"));
         }
     }
 }
